cc579893e80794dc2b05f33fd8c89578c54e65c8
utkarsh684 <shuklautkarsh406@gmail.com>
2025-07-15 21:19:21 +0530

    initial commit

---
+++ DocuVault/src/main/java/com/example/DocuVault/DocuVaultApplication.java
@@ -0,0 +1,13 @@
+package com.example.DocuVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocuVaultApplication.class, args);
+	}
+
+}
---
+++ DocuVault/src/test/java/com/example/DocuVault/DocuVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DocuVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
